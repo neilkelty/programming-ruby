107be37ab2536cc41f6218de746701eea9ec283e
Neil Kelty <neil@neilkelty.com>
2013-04-21 10:11:05 -0400

    Refactor top five block

--- chapter_4/word_freq/word_freq.rb
+++ chapter_4/word_freq/word_freq.rb
@@ -8,8 +8,6 @@ counts = count_frequency(word_list)
 sorted = counts.sort_by { |word, count| count }
 top_five = sorted.last(5)
 
-for i in 0...5
-  word = top_five[i][0]
-  count = top_five[i][1]
+top_five.each do |word, count|
   puts "#{word}: #{count}"
 end
